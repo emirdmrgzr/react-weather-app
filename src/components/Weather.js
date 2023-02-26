a498dfd29e8d412663c8e4b4b6ae2de5253da559
emirdmrgzr <emir.demirgezer@hotmail.com>
2023-02-26 14:21:37 +0300

    Style and function update

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,17 +3,16 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
  const Weather = ()=>{
+    
+    const [data,setData] = useState({});
     const [city,setCity] = useState("");
-    const [shownCity,setShownCity] = useState("");
-    const [weatherDeg,setWeatherDeg] = useState(0);
-    const handleChange = (e)=>{
-      setCity(e.target.value);
-    }
-    const handleClick = async(e) =>{
-      e.preventDefault();
-      await getData();
-      await setShownCity(city.toLowerCase().split(" ").map((city) => city.charAt(0).toUpperCase() + city.slice(1)).join());
-      await setCity("");
+
+    const handleEnter = async(e)=>{
+      if(e.key === "Enter"){
+        e.preventDefault();
+        await getData();
+        await setCity("");
+      }
     }
 
     const  getData = async() =>{
@@ -21,25 +20,73 @@ import axios from "axios";
     .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bbb20ad16f1fa337b2f3086d9f6e60a0`)
     //.then(response => setWeatherDeg(Math.round(response.data.main.temp-273) + " Degree"))
     .then((response) =>{
+      setData(response.data);
       console.log(response.data);
     })
     .catch(error => console.log(error))
     }
+
   return(
     <div className="container">
         <div className="wrapper">
-          <h1 className="page-header">SIMPLE WEATHER APP</h1>
           <form className="form">
-            <input type="text" id="cityInput" placeholder="Enter a City" value={city} onChange={(e) =>setCity(e.target.value)}  />
-            <button onClick={handleClick} type="submit" value="Submit">Search</button>
+            <input 
+            type="text" 
+            id="cityInput" 
+            placeholder="Enter a City" 
+            value={city} 
+            onKeyDown={handleEnter}
+            onChange={(e) =>setCity(e.target.value)}  
+            />
           </form>
           <div className="city-weather">
-          <div className="el1">Current City : {shownCity}</div>
-          <div className="el2">Current Weather  : {weatherDeg}</div>
-
+            <div className="primaries">
+              <div className="location"><p>{data.name}</p></div>
+              <div className="degree">{data.main ? <p>{Math.round(data.main.temp-273)}°C</p> : null}</div>
+              <div className="condition">{data.main ? <p>{data.weather[0].main}</p> : null}</div>
+            </div>
+            <div className="others">
+              <div className="humidity">
+                <p>Humidity</p>
+                {data.main ? <p> {data.main.humidity + "%"}</p> : null}
+              </div>
+              <div className = "wind">
+                <p>Wind</p> 
+                {data.main ? <p>{data.wind.speed} </p> : null}
+              </div>
+            </div>
           </div>
         </div>
     </div>
 )
 }
 export default Weather;
+
+
+
+
+/*
+Instead of using the name of the location in the api I could show the corrected version of the input user entered as you can see below.
+
+const [shownCity,setShownCity] = useState("");
+await setShownCity(city.toLowerCase().split(" ").map((city) => city.charAt(0).toUpperCase() + city.slice(1)).join());  <div className="el1">Current City : {shownCity}</div>
+
+*/
+
+/*
+  We can also set a button as below with handleClick function:
+
+  const handleClick = async(e) =>{
+      e.preventDefault();
+      await getData();
+      await setCity("");
+    }
+  
+  <button 
+  onClick={handleClick} 
+  type="submit" 
+  value="Submit"
+  >
+  Search
+  /button>
+*/
